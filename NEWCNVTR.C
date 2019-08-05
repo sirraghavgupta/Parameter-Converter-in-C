6bdc44f3c20d830a3365b462e455271f8920827b
sirraghavgupta <raghav.mhc@gmail.com>
2019-08-05 13:05:22 -0400

    compiler specific changes done

--- NEWCNVTR.C
+++ NEWCNVTR.C
@@ -1,82 +1,92 @@
+/*##############################################################################
+AUTHOR : RAGHAV GUPTA
+DATE   : 5 august 2019
+AIM    : convert a quantity from one unit to another 
+STATUS : !!!  !!!
+##############################################################################*/
 
 #include<stdio.h>
+#include <process.h>
 #include"newcon.h"
 #include"weight.h"
 #include"temp.h"
-void main()
+
+int main()
 {
-char ch,choice;int source,destination,input,in,out,output;
-do
-{
-clrscr();
-printf("\n\t\t\t   Welcome to Convertor\n\n\n");
-printf("\nEenter ur choice:-\n");
-printf("\n1.CURRENCY CONVERTOR\n");
-printf("\n2.WEIGHT CONVERTOR\n");
-printf("\n3.TEMPERATURE CONVERTOR\n");
-printf("\n4.QUIT");
-ch=getch();
-switch(ch)
+	char ch,choice;int source,destination,input,in,out,output;
+	do
 	{
-	case '1':do
-		 {
-		 source=currency_convertor_in();
-		 if(source==9)
-			{
+	// clrscr();
+	printf("\n\t\t\t   Welcome to Convertor\n\n\n");
+	printf("\nEenter ur choice:-\n");
+	printf("\n1.CURRENCY CONVERTOR\n");
+	printf("\n2.WEIGHT CONVERTOR\n");
+	printf("\n3.TEMPERATURE CONVERTOR\n");
+	printf("\n4.QUIT");
+	ch=getch();
+	switch(ch)
+		{
+		case '1':do
+			 {
+			 source=currency_convertor_in();
+			 if(source==9)
+				{
+				 break;
+				}
+			 destination=currency_convertor_out();
+			 if(destination==9)
+				{
+				 break;
+				}
+			 currency_convertor_pro(source,destination);
+			 printf("\npress y to continue with currency convertor\n");
+			 choice=getch();
+			 }while(choice=='y'||choice=='Y');
 			 break;
-			}
-		 destination=currency_convertor_out();
-		 if(destination==9)
-			{
-			 break;
-			}
-		 currency_convertor_pro(source,destination);
-		 printf("\npress y to continue with currency convertor\n");
-		 choice=getch();
-		 }while(choice=='y'||choice=='Y');
-		 break;
 
-	case '2':do
-		 {
-		 input=weight_convertor_in();
-		 if(input==7)
-			{
+		case '2':do
+			 {
+			 input=weight_convertor_in();
+			 if(input==7)
+				{
+				 break;
+				}
+			 output=weight_convertor_out();
+			 if(output==7)
+				{
+				 break;
+				}
+			 weight_convertor_pro(input,output);
+			 printf("\npress y to continue with weight convertor\n");
+			 choice=getch();
+			 }while(choice=='y'||choice=='Y');
 			 break;
-			}
-		 output=weight_convertor_out();
-		 if(output==7)
-			{
+
+		case '3':do
+			 {
+			 in=temp_convertor_in();
+			 if(in==5)
+				{
+				 break;
+				}
+			 out=temp_convertor_out();
+			 if(out==5)
+				{
+				 break;
+				}
+			 temp_convertor_pro(in,out);
+			 printf("\npress y to continue with temperature convertor\n");
+			 choice=getch();
+			 }while(choice=='y'||choice=='Y');
 			 break;
-			}
-		 weight_convertor_pro(input,output);
-		 printf("\npress y to continue with weight convertor\n");
-		 choice=getch();
-		 }while(choice=='y'||choice=='Y');
-		 break;
 
-	case '3':do
-		 {
-		 in=temp_convertor_in();
-		 if(in==5)
-			{
-			 break;
-			}
-		 out=temp_convertor_out();
-		 if(out==5)
-			{
-			 break;
-			}
-		 temp_convertor_pro(in,out);
-		 printf("\npress y to continue with temperature convertor\n");
-		 choice=getch();
-		 }while(choice=='y'||choice=='Y');
-		 break;
+		case '4': exit(0);
 
-	case '4': exit(0);
+		}
+	//printf("\npress y to go to previous menu\n");
+	//ch=getch();
 
-	}
-//printf("\npress y to go to previous menu\n");
-//ch=getch();
+	}while(1/*ch=='y'||ch=='Y'*/);
 
-}while(1/*ch=='y'||ch=='Y'*/);
+return 0;
 }
